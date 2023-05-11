46b3f8610d9f5bdbf81fd1d2b8846b00c5be3dda
Daniel Alexander Muelas Rivera <dmuelas@protecnicaing.com>
2023-05-11 15:12:07 -0500

    Realizando la configuración del diseño principal

--- control-accesos/resources/views/home.blade.php
+++ control-accesos/resources/views/home.blade.php
@@ -1,23 +1,140 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="pagetitle">
+    <h1>Dashboard</h1>
+    <nav>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.html">Inicio</a></li>
+            <li class="breadcrumb-item active">Dashboard</li>
+        </ol>
+    </nav>
+</div>
+<!-- End Page Title -->
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+<section class="section dashboard">
+    <div class="row">
+        <!-- Left side columns -->
+        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 col-xxl-6">
+            <div class="row">
+                <!-- Main panel -->
+                <div class="card">
+                    <div class="card-body card-main px-5">
+                        <h5 class="card-title"> {{ __('Panel de control') }}
+                            <span>| {{ __('Colaboradores') }}</span>
+                        </h5>
+                        <hr>
+                        <div class="activity">
+
+                            <div class="search">
+                                <div class="input-group">
+                                    <span class="input-group-text">
+                                        <img src="{{ asset('images/pisa.png') }}" class="company-logo">
+                                    </span>
+
+                                    <input class="form-control" list="datalistOptions" id="exampleDataList"
+                                        placeholder="Buscar colaborador, visitante ...">
+
+                                    <datalist id="datalistOptions">
+                                        <option value="Fabian Riquet">
+                                        <option value="Raquel Perez">
+                                        <option value="Jaider Vasquez">
+                                        <option value="Daniel Muelas">
+                                    </datalist>
+
+                                    <span class="input-group-text">
+                                        <i class="bi bi-search"></i>
+                                    </span>
+                                </div>
+                            </div>
+
+                            <!-- User Information -->
+                            <div class="row">
+                                <div class="col-4 mt-4">
+                                    <div class="user-image">
+                                        <img src="{{ asset('images/daniel_muelas.jpg') }}" alt="">
+                                    </div>
+                                </div>
+
+                                <div class="align-self-center col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-4">
+                                    <div class="card-title">
+                                        DANIEL ALEXANDER MUELAS RIVERA
+                                    </div>
+                                    <div>
+                                        <i class="bi bi-person-vcard-fill"></i> <span> 1144097956 </span>
+                                    </div>
+                                    <div>
+                                        <i class="bi bi-geo-alt-fill"></i> <span> Técnologia, Edificio Antiguo </span>
+                                    </div>
+                                    <div>
+                                        <i class="bi bi-person-fill"></i> <span> Coordinador de Técnologia </span>
+                                    </div>
+                                </div>
+                                <div class="col-4 mt-2 align-self-center">
+                                    <a href="" class="dashboard-user-door door-in">
+                                        <div class="content-icon"> <i class="bi bi-box-arrow-right"></i> </div>
+                                        <div class="content-text"> {{ __('Ingreso peatonal') }} </div>
+                                    </a>
+
+                                    <a href="" class="dashboard-user-door door-out mt-2">
+                                        <div class="content-icon"> <i class="bi bi-box-arrow-left"></i> </div>
+                                        <div class="content-text"> {{ __('Salida peatonal') }} </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <!-- End user information -->
+                            <hr>
+                            <div class="d-flex justify-content-center">
+
+                                <a href="" class="dashboard-user-action user-action-primary m-1">
+                                    <div class="content-icon"> <i class="bi bi-person-fill-add"></i> </div>
+                                    <div class="content-text"> {{ __('Registrar visitante') }} </div>
+                                </a>
+
+                                <a href="" class="dashboard-user-action user-action-warning m-1">
+                                    <div class="content-icon"> <i class="bi bi-person-video"></i> </div>
+                                    <div class="content-text"> {{ __('Ver colaborador') }} </div>
+                                </a>
+
+                                <a href="" class="dashboard-user-action user-action-success m-1">
+                                    <div class="content-icon"> <i class="bi bi-person-fill-check"></i> </div>
+                                    <div class="content-text"> {{ __('Ver permisos') }} </div>
+                                </a>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
                 </div>
+                <!-- End main Panel-->
             </div>
         </div>
+        <!-- End Left side columns -->
+
+        <!-- Right side columns -->
+        <div class="col-lg-6">
+            <!-- Visitors today -->
+            <div class="card">
+                <div class="card-body card-summary">
+                    <h5 class="card-title"> {{ __('Visitantes') }} <span>| {{ __('Hoy') }}</span></h5>
+                    <hr>
+                    <div class="activity">
+                    </div>
+                </div>
+            </div>
+            <!-- End  Visitors Today-->
+
+            <!-- Visitors today -->
+            <div class="card">
+                <div class="card-body card-summary">
+                    <h5 class="card-title"> {{ __('Permisos') }} <span>| {{ __('Hoy') }}</span></h5>
+                    <hr>
+                    <div class="activity">
+                    </div>
+                </div>
+            </div>
+            <!-- End  Visitors Today-->
+
+        </div>
+        <!-- End Right side columns -->
     </div>
-</div>
+</section>
 @endsection
--- control-accesos/resources/views/layouts/app.blade.php
+++ control-accesos/resources/views/layouts/app.blade.php
@@ -1,105 +1,312 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <!-- Favicons -->
+    <link href="" rel="icon">
+    <link href="" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{ asset('images/pisa.png') }}">
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <!-- ======= Header ======= -->
+    <header id="header" class="header fixed-top d-flex align-items-center">
 
-                    <!-- Left Side Of Navbar -->
-                    <div class="vr"></div>
+        <div class="d-flex align-items-center justify-content-between">
+            <a href="{{ route('home') }}" class="logo d-flex align-items-center">
+                <img src="{{ asset('images/pisa.png') }}" alt="">
+                <span class=""> Protécnica Ingeniería </span>
+            </a>
+            <i class="bi bi-list toggle-sidebar-btn"></i>
+        </div>
+        <!-- End Logo -->
 
-                    <ul class="navbar-nav me-auto">
+        <div class="search-bar">
+            <form class="search-form d-flex align-items-center" method="POST" action="#">
+                <input type="text" name="query" placeholder="Buscar" title="Enter search keyword">
+                <button type="submit" title="Search"><i class="bi bi-search"></i></button>
+            </form>
+        </div>
+        <!-- End Search Bar -->
 
-                        <div class="m-2">
-                            <select class="form-select form-select" aria-label=".form-select-lg example">
-                                <option selected>Protecnica Ingenieria</option>
-                                <option value="1">Nutresol</option>
-                                <option value="2">Quimica Lider</option>
-                            </select>
-                        </div>
+        <nav class="header-nav ms-auto">
+            <ul class="d-flex align-items-center">
 
-                        <div class="vr"></div>
+                <li class="nav-item d-block d-lg-none">
+                    <a class="nav-link nav-icon search-bar-toggle " href="#">
+                        <i class="bi bi-search"></i>
+                    </a>
+                </li><!-- End Search Icon-->
 
-                        <div class="search m-2">
-                            <div class="input-group">
-                                <span class="input-group-text">
-                                    <img src="{{ asset('images/pisa.png') }}" alt="" srcset="">
-                                </span>
+                <li class="nav-item dropdown">
 
-                                <input class="form-control" list="datalistOptions" id="exampleDataList"
-                                    placeholder="Buscar colaborador, visitante ...">
+                    <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+                        <i class="bi bi-bell"></i>
+                        <span class="badge bg-primary badge-number">4</span>
+                    </a><!-- End Notification Icon -->
 
-                                <datalist id="datalistOptions">
-                                    <option value="Daniel Muelas - 1144097956">
-                                </datalist>
+                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
+                        <li class="dropdown-header">
+                            You have 4 new notifications
+                            <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
 
-                                <span class="input-group-text">
-                                    <i class="bi bi-search"></i>
-                                </span>
-                            </div>
-                        </div>
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} {{ Auth::user()->last_name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
+                        <li class="notification-item">
+                            <i class="bi bi-exclamation-circle text-warning"></i>
+                            <div>
+                                <h4>Lorem Ipsum</h4>
+                                <p>Quae dolorem earum veritatis oditseno</p>
+                                <p>30 min. ago</p>
                             </div>
                         </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div>
-            <nav>
-                <div class="item-menu">
-                </div>
-            </nav>
-            <main class="py-4">
-                @yield('content')
-            </main>
+
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+
+                        <li class="notification-item">
+                            <i class="bi bi-x-circle text-danger"></i>
+                            <div>
+                                <h4>Atque rerum nesciunt</h4>
+                                <p>Quae dolorem earum veritatis oditseno</p>
+                                <p>1 hr. ago</p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+
+                        <li class="notification-item">
+                            <i class="bi bi-check-circle text-success"></i>
+                            <div>
+                                <h4>Sit rerum fuga</h4>
+                                <p>Quae dolorem earum veritatis oditseno</p>
+                                <p>2 hrs. ago</p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+
+                        <li class="notification-item">
+                            <i class="bi bi-info-circle text-primary"></i>
+                            <div>
+                                <h4>Dicta reprehenderit</h4>
+                                <p>Quae dolorem earum veritatis oditseno</p>
+                                <p>4 hrs. ago</p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li class="dropdown-footer">
+                            <a href="#">Show all notifications</a>
+                        </li>
+
+                    </ul><!-- End Notification Dropdown Items -->
+
+                </li><!-- End Notification Nav -->
+
+                <li class="nav-item dropdown">
+
+                    <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+                        <i class="bi bi-chat-left-text"></i>
+                        <span class="badge bg-success badge-number">3</span>
+                    </a><!-- End Messages Icon -->
+
+                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
+                        <li class="dropdown-header">
+                            You have 3 new messages
+                            <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+
+                        <li class="message-item">
+                            <a href="#">
+                                <img src="{{ asset('images/messages-1.jpg') }}" alt="" class="rounded-circle">
+                                <div>
+                                    <h4>Maria Hudson</h4>
+                                    <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
+                                    <p>4 hrs. ago</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+
+                        <li class="message-item">
+                            <a href="#">
+                                <img src="{{ asset('images/messages-2.jpg') }}" alt="" class="rounded-circle">
+                                <div>
+                                    <h4>Anna Nelson</h4>
+                                    <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
+                                    <p>6 hrs. ago</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+
+                        <li class="message-item">
+                            <a href="#">
+                                <img src="{{ asset('images/messages-3.jpg') }}" alt="" class="rounded-circle">
+                                <div>
+                                    <h4>David Muldon</h4>
+                                    <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
+                                    <p>8 hrs. ago</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+
+                        <li class="dropdown-footer">
+                            <a href="#">Show all messages</a>
+                        </li>
+
+                    </ul><!-- End Messages Dropdown Items -->
+
+                </li><!-- End Messages Nav -->
+
+                <li class="nav-item dropdown pe-3">
+
+                    <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+                        <img src="{{ asset('images/daniel_muelas.jpg') }}" alt="Profile" class="rounded-circle">
+                        <span class="d-none d-md-block dropdown-toggle ps-2">
+                            {{ Auth::user()->name }}
+                        </span>
+                    </a><!-- End Profile Iamge Icon -->
+
+                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+                        <li class="dropdown-header">
+                            <h6>{{ Auth::user()->name }} {{ Auth::user()->last_name }}</h6>
+                            <span>Administrador del sistema</span>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+
+                        <li>
+                            <a class="dropdown-item d-flex align-items-center" href="#">
+                                <i class="bi bi-person"></i>
+                                <span>Mi perfil</span>
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+
+                        <li>
+                            <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                                <i class="bi bi-gear"></i>
+                                <span>Configuración de la cuenta</span>
+                            </a>
+                        </li>
+
+
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+
+                        <li>
+                            <a class="dropdown-item d-flex align-items-center" href="#">
+                                <i class="bi bi-box-arrow-right"></i>
+                                <span> {{ __('Cerrar sesión') }} </span>
+                            </a>
+                        </li>
+
+                    </ul><!-- End Profile Dropdown Items -->
+                </li><!-- End Profile Nav -->
+
+            </ul>
+        </nav><!-- End Icons Navigation -->
+
+    </header><!-- End Header -->
+
+    <!-- ======= Sidebar ======= -->
+    <aside id="sidebar" class="sidebar">
+
+        <ul class="sidebar-nav" id="sidebar-nav">
+
+            <li class="nav-item">
+                <a class="nav-link " href=" {{ route('home') }} ">
+                    <i class="bi bi-house-fill"></i>
+                    <span>Inicio</span>
+                </a>
+            </li><!-- End Dashboard Nav -->
+
+            <li class="nav-item">
+                <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                    <i class="bi bi-menu-button-wide"></i><span>Components</span><i
+                        class="bi bi-chevron-down ms-auto"></i>
+                </a>
+                <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                    <li>
+                        <a href="components-alerts.html">
+                            <i class="bi bi-circle"></i><span>Alerts</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </li><!-- End Components Nav -->
+
+
+            <li class="nav-heading">Pages</li>
+
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="users-profile.html">
+                    <i class="bi bi-person"></i>
+                    <span>Profile</span>
+                </a>
+            </li><!-- End Profile Page Nav -->
+        </ul>
+
+    </aside><!-- End Sidebar-->
+
+    <main id="main" class="main">
+        @yield('content')
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer" class="footer">
+        <div class="copyright">
+            &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
         </div>
+        <div class="credits">
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+    </footer><!-- End Footer -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
 </body>
 
 </html>
